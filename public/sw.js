0297c64b45fe4d61f96308fc1778efc537d89653
Tejeswar <gvtejeswar99@gmail.com>
2026-04-23 22:54:22 +0530

    fixed main.tsx path

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "smart-whiteboard-cache-v1";
+
+const urlsToCache = [
+  "/smart-whiteboard/",
+  "/smart-whiteboard/index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
