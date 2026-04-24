b81a2573146d7cc045ba399d4d1e4518c1ddf8a8
Tejeswar <gvtejeswar99@gmail.com>
2026-04-24 13:07:20 +0530

    added app icon

---
+++ app/src/main/java/io/github/tejeswar4/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.tejeswar4.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
