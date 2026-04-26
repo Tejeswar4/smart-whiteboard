e68b0d4fb822564379a3c480fe9b7254620e8acc
Tejeswar <gvtejeswar99@gmail.com>
2026-04-26 19:45:16 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,22 +1,9 @@
-const CACHE_NAME = "smart-whiteboard-cache-v1";
-
-const urlsToCache = [
-  "/smart-whiteboard/",
-  "/smart-whiteboard/index.html"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", () => {
+  self.clients.claim();
 });
+
+self.addEventListener("fetch", () => {});
